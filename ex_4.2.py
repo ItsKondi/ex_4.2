81229a98bf5c55d8633abc5c8effc1688e67c204
Konrad Jankowski <konrad199907@gmail.com>
2025-04-22 20:54:21 +0200

    have idea

--- ex_4.2.py
+++ ex_4.2.py
@@ -1,2 +1,10 @@
 def is_palindrome(word):
-    
+
+    word_reversed = str(reversed(word))
+    if word == word_reversed:
+        return True
+    else: 
+        return False
+        
+is_palindrome("kajak")
+print(str(reversed("kajak")))
